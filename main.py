117a1ad5c5e13fea18420571c43c6aff18fe45cb
Jada Shepard <jnshepard12@gmail.com>
2023-10-03 13:10:26 -0500

    Created requirements.txt and pygame install

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pygame
 from grid import Grid
 
 def dfs(stack, maze, cell):
@@ -8,9 +9,22 @@ def dfs(stack, maze, cell):
             dfs(stack, maze, neighbor)
     return stack
 
+class Maze:
+    def __init__(self, grid_size) -> None:
+        pygame.init()
+        self.DIS_WIDTH = grid_size
+        self.DIS_HEIGHT = grid_size
+        self.dis = pygame.display.set_mode((self.DIS_WIDTH, self.DIS_HEIGHT))
+        pygame.display.set_caption("Maze")
+        self.maze = Grid(grid_size, grid_size)
+        self.maze.draw()
+    def draw_maze(self):
+        pass
+
 #Main for running Maze Generator and Solver
 if __name__=="__main__":
     maze = Grid(3,3)
+    maze2 = Maze(3)
 
     print("Grid: ")
     maze.print_grid()
